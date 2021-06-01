8e7bf9bae94b514c7104c99c27e7e75b4878adc0
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-06-01 21:19:23 +0200

    Navabar In - Progress 2

--- src/components/general/Navbar.js
+++ src/components/general/Navbar.js
@@ -7,36 +7,48 @@ function Navbar() {
       <div className="navbar-banner">
         <Container
           fluid="xl"
-          style={{  height: "100%", paddingLeft:"0", paddingRight: "0", paddingTop: "10px", backgroundColor:"red"  }}
+          style={{
+            height: "100%",
+            paddingLeft: "0",
+            paddingRight: "0",
+            paddingTop: "10px",
+            // backgroundColor: "red"
+          }}
         >
-          <Row style={{}}>
-            <Col style={{}}>
+          <Row>
+            <Col xs={4} style={{ padding: "0" }}>
               <p className="logo-banner">SIZZASNEAKS</p>
             </Col>
 
-            <Col style={{}}>
+            <Col xs={5} style={{ padding: "0" }}>
               <div className="search-bar-box">
                 <input id="search-input" type="text" value="Search" />
               </div>
             </Col>
 
-            <Col  >
+            <Col xs={3} style={{ padding: "0" }}>
               <Row className="top-right-nav-banner-links">
-                <Col xs={2} className="top-right-nav-banner-link" >
+                <Col
+                  xs={2}
+                  className="top-right-nav-banner-link"
+                  style={{ marginLeft: "auto" }}
+                >
                   <p>LogIn</p>
                 </Col>
-                <Col xs={1}  >
+                <Col xs={1}>
                   <div className="vertical-divider"></div>
                 </Col>
-                <Col xs={4} className="top-right-nav-banner-link" >
+                <Col xs={3} className="top-right-nav-banner-link">
                   <p>Sign Up</p>
                 </Col>
-                <Col xs={1}  >
+                <Col xs={1}>
                   <div className="vertical-divider"></div>
                 </Col>
-                <Col xs={3} className="top-right-nav-banner-link" >
+                <Col xs={3} className="top-right-nav-banner-link">
                   <div className="shopping-cart-banner">
-                    <span style={{float:"left"}} className="material-icons">shopping_cart</span>
+                    <span style={{ float: "left" }} className="material-icons">
+                      shopping_cart
+                    </span>
                     <p>Cart</p>
                   </div>
                 </Col>
@@ -48,12 +60,41 @@ function Navbar() {
       <div className="navbar-links-segment">
         <Container
           fluid="xl"
-          style={{  height: "100%" }}
+          style={{ height: "100%", paddingLeft: "0", paddingRight: "0" }}
         >
-          <p>SIZZASNEAKS</p>
+          <div style={{ display: "flex", height: "100%" }}>
+            <div style={{ height: "100%" }}>
+              <NavabarNavigationLink label="BRANDS" />
+              <NavabarNavigationLink label="WOMEN" />
+              <NavabarNavigationLink label="MEN" />
+              <NavabarNavigationLink label="KIDS" />
+            </div>
+            <div
+              style={{
+                height: "100%",
+                marginLeft: "auto",
+              }}
+            >
+              <NavabarNavigationLink label="NEW ARRIVALS" />
+              <NavabarNavigationLink label="WISHLIST" />
+            </div>
+          </div>
         </Container>
       </div>
     </nav>
+  );
+}
+
+function NavabarNavigationLink(props) {
+  return (
+    <div
+      className="navbarNavigationLink"
+      onClick={function () {
+        alert("Coming Soon");
+      }}
+    >
+      <p style={{}}>{props.label}</p>
+    </div>
   );
 }
 
