9fe47e5fc9e0a809cd8f8128e303b057d705141b
Ameer Mahmood <hmah@student.monash.edu>
2021-06-02 14:21:58 +0200

    Products page version 1

---
+++ src/components/data/product_data.js
@@ -0,0 +1,75 @@
+const product_card = [
+    {
+        id: 1,
+        product_name: "Air force 1",
+        brand: "Nike",
+        price: 250,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    },
+    {
+        id: 2,
+        product_name: "Air Jordan 1 ",
+        brand: "Nike",
+        price: 250,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    },
+    {
+        id: 3,
+        product_name: "Air force 1",
+        brand: "Nike",
+        price: 350,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    },
+    {
+        id: 4,
+        product_name: "Air force 1",
+        brand: "Nike",
+        price: 350,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    },
+    {
+        id: 5,
+        product_name: "Air force 1",
+        brand: "Nike",
+        price: 250,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    },
+    {
+        id: 6,
+        product_name: "Air force 1",
+        brand: "Nike",
+        price: 350,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    },
+    {
+        id: 7,
+        product_name: "Air force 1",
+        brand: "Nike",
+        price: 350,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    },
+    {
+        id: 8,
+        product_name: "Air force 1",
+        brand: "Nike",
+        price: 250,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    },
+    {
+        id: 9,
+        product_name: "Air force 1",
+        brand: "Nike",
+        price: 350,
+        currency: "R",
+        thumb: "./images/1.jpg"
+    }
+]
+export default product_card;
--- src/components/pages/ProductsPage.js
+++ src/components/pages/ProductsPage.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from 'react'
+import product_card from "../data/product_data";
 
-function ProductsPage(){
-    return(<p>ProductsPage</p>)
+const ProductsPage = () => {
+    console.log(product_card);
+    const listItems = product_card.map((item) =>
+        <div className="card" key={item.id}>
+            <div className="card_img">
+                <img src={item.thumb} />
+            </div>
+            <div className="card_header">
+                <h2>{item.product_name}</h2>
+                <p>{item.brand}</p>
+                <p className="price">{item.currency}<span>{item.price}</span><div className="btn">Add to cart</div></p>
+
+            </div>
+        </div>
+
+    );
+    return (
+        <div className="products_page">
+            {listItems}
+        </div>
+    )
 }
-
 export default ProductsPage;
