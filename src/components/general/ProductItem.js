c1f0405bf66a4a046a17bfda5046fd26fb4fff13
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-07-08 04:21:57 +0200

    Home Page Screen Started

---
+++ src/components/general/ProductItem.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+import ProductItemImage from "../../images/product-item-image.png";
+
+function ProductItem(props) {
+  return (
+    <div className="product-item">
+      <Row>
+        <Col>
+          <img
+            className="product-item-image"
+            src={ProductItemImage}
+            alt="Product"
+          />
+        </Col>
+      </Row>
+      <Row className="name-rating-row row">
+        <Col>
+          {" "}
+          <p>Jordan 11</p>
+        </Col>
+        <Col xl={2}>
+          <p>4.2</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Nike</p>
+        </Col>
+      </Row>
+      <Row className="name-rating-row row">
+        <Col>
+          {" "}
+          <p>R1,250</p>
+        </Col>
+        <Col xl={5} style={{ padding: "0px" }}>
+          <div
+            style={{
+              backgroundColor: "#FADA35",
+              fontSize: "12px",
+              fontWeight: "bold",
+              textAlign: "center",
+              marginTop: "3px",
+            }}
+          >
+            <p>ADD TO CART</p>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ProductItem;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,13 +1,77 @@
-import React from 'react';
-import LogInComponent from '../general/LogInComponent';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
 
-function HomePage(){
-    return(
-        <div> 
-        <p>HomePage</p>
-            <LogInComponent/>
-        </div>  
-    )
+// import LogInComponent from '../general/LogInComponent';
+
+import ProductItem from "../general/ProductItem";
+
+//Replace With Image From the internet
+import PromotionlBannerImgOne from "../../images/homepage-promotion-banner-one.png";
+import PromotionlBannerImgTwo from "../../images/homepage-promotion-banner-image-two.png";
+import PromotionalBannerImgThree from "../../images/homepage-promotion-banner-image-three.png";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <Row>
+        <img
+          className="homepage-promotion-banner-image-one"
+          src={PromotionlBannerImgOne}
+          alt="Promotional Banner One"
+        />
+      </Row>
+      <Row>
+        <div className="home-section-header">
+          <p>NEW ARRIVALS</p>
+          <p style={{ marginLeft: "10px" }}>See All</p>
+        </div>
+      </Row>
+      <Row className="homepage-banner-product-segment">
+        <Col xl={6}>
+          <img
+            className="homepage-promotion-banner-image-two"
+            src={PromotionlBannerImgTwo}
+            alt="Promotional Banner One"
+          />
+        </Col>
+        <Col xl={3} style={{ display: "flex", justifyContent: "center"}}>
+          <ProductItem />
+        </Col>
+        <Col xl={3} style={{ display: "flex", justifyContent: "center"}} >
+          <ProductItem />
+        </Col>
+      </Row>
+
+      <Row className="homepage-banner-product-segment">
+        <Col xl={3} style={{ display: "flex", justifyContent: "center"}} >
+          <ProductItem />
+        </Col>
+        <Col xl={3} style={{ display: "flex", justifyContent: "center"}} >
+          <ProductItem />
+        </Col>
+        <Col xl={6}>
+          <img
+            className="homepage-promotion-banner-image-two"
+            src={PromotionalBannerImgThree}
+            alt="Promotional Banner One"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <div className="home-section-header">
+          <p>TOP SELLERS</p>
+          <p style={{ marginLeft: "10px" }}>See All</p>
+        </div>
+      </Row>
+
+      <Row className="product-coursel">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </Row>
+    </div>
+  );
 }
 
 export default HomePage;
