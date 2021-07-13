3f5683c71a278c949aa39e07e91238ac6efff914
Ameer Mahmood <hmah@student.monash.edu>
2021-07-13 10:37:04 +0200

    products page done

--- src/components/general/Navbar.js
+++ src/components/general/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
 function Navbar() {
   return (
     <nav>
--- src/components/pages/ProductsPage.js
+++ src/components/pages/ProductsPage.js
@@ -1,29 +1,86 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import ProductItem from "../general/ProductItem.js";
+import { Grid } from "@material-ui/core";
 
 function ProductsPage() {
   return (
     <>
-      <Row>
-        <Col>
+      <Grid container spacing={5}>
+        <Grid item xs={20} sm={5} md={3}>
           <div className="products-display">
             <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem /><ProductItem />
-            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
             <ProductItem />
           </div>
-        </Col>{" "}
-      </Row>
-      <Row>
-        <Col> <div className="page-number-indicator-section">
-        <PageNumberHolder />
-        <PageNumberHolder />
-        <PageNumberHolder />
-      </div></Col>{" "}
-      </Row>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+        <Grid item xs={20} sm={5} md={3}>
+          <div className="products-display">
+            <ProductItem />
+          </div>
+        </Grid>
+      </Grid>
+
+      <Grid container>
+        <div className="page-number-indicator-section">
+          <PageNumberHolder/>
+          <PageNumberHolder2/>
+          <PageNumberHolder3/>
+          <PageNumberHolder4/>
+          <PageNumberHolder5/>
+          <PageNumberHolder6/>
+
+
+        </div>
+      </Grid>
     </>
   );
 }
@@ -32,8 +89,43 @@ export default ProductsPage;
 
 function PageNumberHolder() {
   return (
-    <div className="page-number-holder">
+    <div className="page-number-holder-1">
       <p>1</p>
     </div>
   );
 }
+function PageNumberHolder2() {
+    return (
+      <div className="page-number-holder-2">
+        <p>2</p>
+      </div>
+    );
+  }
+  function PageNumberHolder3() {
+    return (
+      <div className="page-number-holder-2">
+        <p>3</p>
+      </div>
+    );
+  }
+  function PageNumberHolder4() {
+    return (
+      <div className="page-number-holder-2">
+        <p>4</p>
+      </div>
+    );
+  }
+  function PageNumberHolder5() {
+    return (
+      <div className="page-number-holder-2">
+        <p>{">"}</p>
+      </div>
+    );
+  }
+  function PageNumberHolder6() {
+    return (
+      <div className="page-number-holder-2">
+        <p>{">>"}</p>
+      </div>
+    );
+  }
