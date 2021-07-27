91030c76e78167352f7f6d900113146677bea6a6
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-07-27 20:16:05 +0200

    Progress

--- src/components/pages/ProductPage.js
+++ src/components/pages/ProductPage.js
@@ -3,9 +3,12 @@ import React, { useState } from "react";
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css'; */
 
+import Styles from "./ProductPage.module.css";
+import { Row, Col } from "react-bootstrap";
+
 import { Grid, Typography } from "@material-ui/core";
 import ImageGrid from "../general/ImageGrid";
-import MainImage from "../general/MainImage";
+//import MainImage from "../general/MainImage";
 import Info from "../general/Info";
 import Reviews from "../general/Reviews";
 import ProductItem from "../general/ProductItem.js";
@@ -32,30 +35,77 @@ const reviews = {
 
 function ProductPage() {
   const [selectedImage, setSelectedImage] = useState(0);
+
   return (
     <>
       <div>
-        <Grid
-          container
-          spacing={1}
-          style={{ maxWidth: 1100, margin: "0 auto" }}
-        >
-          <Grid Item sm={1}>
-            <ImageGrid
-              images={images}
-              onSelect={setSelectedImage}
-              selectedImage={selectedImage}
-            />
-          </Grid>
-          <Grid Item sm={5}>
-            <MainImage src={images[selectedImage]} />
-          </Grid>
-          <Grid Item sm={6}>
-            <Info {...product} />
-          </Grid>
-        </Grid>
+        <div className={Styles.currentProductCard}>
+          <Row>
+            <Col className={Styles.currentProductImagesSegment}>
+              <Row>
+                <Col className={Styles.imageOptionsSegment} xl={2}>
+                  {" "}
+                  <ImageGrid
+                    images={images}
+                    onSelect={setSelectedImage}
+                    selectedImage={selectedImage}
+                  />
+                </Col>
+                <Col className={Styles.mainImageSegment} xl={10}>
+                  <Grid Item sm={5}>
+                    <MainImage src={images[selectedImage]} />
+                  </Grid>
+                </Col>
+              </Row>
+            </Col>
+            <Col className={Styles.currentProductDetailsSegment}>
+              <Row>
+                <Col>Brand</Col>
+              </Row>
+              <Row>
+                <Col>Product Name</Col>
+              </Row>
+              <Row>
+                <Col>Product Description</Col>
+              </Row>
+              <Row>
+                <Col>Product Rating</Col>
+              </Row>
+              <Row>
+                <Col>Color Selector</Col>
+              </Row>
+              <Row>
+                <Col>Size Selector</Col>
+              </Row>
+              <Row>
+                <Col>Price</Col>
+                <Col>ADD TO CART ADD TO WISHLIST</Col>
+              </Row>
+            </Col>
+          </Row>
 
-        <Grid
+          {/* <Grid
+            container
+            spacing={1}
+            style={{ maxWidth: 1100, margin: "0 auto" }}
+          >
+            <Grid Item sm={1}>
+              <ImageGrid
+                images={images}
+                onSelect={setSelectedImage}
+                selectedImage={selectedImage}
+              />
+            </Grid>
+            <Grid Item sm={5}>
+              <MainImage src={images[selectedImage]} />
+            </Grid>
+            <Grid Item sm={6}>
+              <Info {...product} />
+            </Grid>
+          </Grid> */}
+        </div>
+
+        {/* <Grid
           container
           spacing={1}
           style={{ maxWidth: 1100, margin: "0 auto" }}
@@ -63,13 +113,19 @@ function ProductPage() {
           <Grid Item>
             <Reviews {...reviews} />
           </Grid>
-        </Grid>
+        </Grid> */}
 
         <div className="w-100"></div>
       </div>
       <ProductCarousel label="Related Products" />
     </>
   );
+}
+
+ function MainImage({ src }){
+  return <div>
+      <img src={src} width="100%"/>
+  </div>;
 }
 
 export default ProductPage;
