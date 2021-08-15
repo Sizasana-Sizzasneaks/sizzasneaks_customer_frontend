5f3342e434fe23ab9f8653c68f3cd6e430693f97
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-08-15 22:17:21 +0200

    B2CartDelete

--- src/api/cart.js
+++ src/api/cart.js
@@ -80,7 +80,7 @@ export const getCart = async () => {
     }
     // Add to Cart Logic
   } else {
-    return { ok: false, message: "Add to Cart Failed - Try again" };
+    return { ok: false, message: "Getting Cart Failed - Try again" };
   }
 };
 
@@ -124,6 +124,46 @@ export const updateCartItemQuantity = async (
     }
     // Add to Cart Logic
   } else {
-    return { ok: false, message: "Add to Cart Failed - Try again" };
+    return { ok: false, message: "Updating a Cart Item Failed - Try again" };
+  }
+};
+
+export const deleteSingleCartItem = async (product_id, option) => {
+  if (isLoaded(store.getState().firebase.auth)) {
+    if (!isEmpty(store.getState().firebase.auth)) {
+      var getTokenResult = await getCurrentUserIdToken();
+
+      if (getTokenResult.ok === true) {
+        const config = {
+          headers: {
+            credentialclaims: "customer",
+            Authorization: "Bearer " + getTokenResult.data,
+          },
+          data: {
+            product_id: product_id,
+            option: option,
+          },
+        };
+        return axios
+          .delete(API_CONSTANTS.CART_ROUTE + "/cart_item", config)
+          .then((res) => {
+            // Request Succesfull
+            //Handle Different HTTP Status Codes and Responses
+            return res.data;
+          })
+          .catch((error) => {
+            //Request Unsuccesfull
+            return { ok: false, error: error };
+          });
+      } else {
+        //Failed to get Token
+        return getTokenResult;
+      }
+    } else {
+      return { ok: false, message: "No User Signed In" };
+    }
+    // Add to Cart Logic
+  } else {
+    return { ok: false, message: "Deleting a Cart Item Failed - Try again" };
   }
 };
