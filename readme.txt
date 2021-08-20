e396686e9506be54497ef9ec65cc07f0c62a3486
Matamando-Kalilani <matamandokalilani@outlook.com>
2021-08-20 06:05:48 +0200

    Done

---
+++ readme.txt
@@ -0,0 +1,9 @@
+This web application serves as one of the front end applications within the sizzasneaks e-commerce
+platform
+
+This is the Customer platform where customers will be able to create an account, view products and
+make purchases on those items. 
+
+This web application completes its functionality tasks by making HTTP requests to the back-end application
+all system business logic resides. 
+
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,16 +2,20 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import Reducers from "./reducers/index.js";
 import { composeWithDevTools } from "redux-devtools-extension";
-import {getFirebase, reactReduxFirebase} from "react-redux-firebase";
-import firebase from "../config/firebaseConfig.js"
+import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
+import firebase from "../config/firebaseConfig.js";
 
+//a redux store is being created to store the state of the application using the reducers
+
+//the redux dev tool extension wraps the actions with its own acton.
 
 const store = createStore(
   Reducers,
-   +window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  composeWithDevTools(
-    applyMiddleware(thunk.withExtraArgument({getFirebase}))
-  )
+  +window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeWithDevTools(applyMiddleware(thunk.withExtraArgument({ getFirebase })))
 );
 
+//arguments are passed to the createstore function
+//thunk is being used as a redux enhancer to allow us to work with asynchronous actions
+//the store is then exported
 export default store;
